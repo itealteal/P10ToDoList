510ca17639b9a4e956e568870f73d0640df1d4a6
20033824 <20033824@myrp.edu.sg>
2021-07-27 10:39:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/todolist/CustomAdapter.java
@@ -0,0 +1,55 @@
+package com.example.todolist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomAdapter extends ArrayAdapter {
+    Context parent_context;
+    int layout_id;
+    ArrayList<ToDoItem> versionList;
+
+
+    public CustomAdapter(Context context, int resource, ArrayList<ToDoItem> objects) {
+        super(context, resource, objects);
+
+        parent_context = context;
+        layout_id = resource;
+        versionList = objects;
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Obtain the LayoutInflater object
+        LayoutInflater inflater = (LayoutInflater)
+                parent_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        // "Inflate" the View for each row
+        View rowView = inflater.inflate(layout_id, parent, false);
+
+        // Obtain the UI components and do the necessary binding
+        TextView tvName = rowView.findViewById(R.id.tvName);
+        TextView tvVersion = rowView.findViewById(R.id.tvVersion);
+
+        // Obtain the Android Version information based on the position
+        ToDoItem currentItem = versionList.get(position);
+
+        // Set values to the TextView to display the corresponding information
+        tvName.setText(currentItem.getTitle());
+        tvVersion.setText(currentItem.toString());
+
+        return rowView;
+    }
+
+}
+
